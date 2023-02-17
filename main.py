0f7889ebd1dc066386361eba627a5c3729dfaaf6
Егор Давыдов <113803436+EgoricDavidenko@users.noreply.github.com>
2023-02-17 18:15:52 +0300

    без pip install -r requirements.txt

---
+++ main.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+con = sqlite3.connect("coffee.db")
+
+cur = con.cursor()
+
+result = cur.execute("""SELECT * FROM cofeyok""").fetchall()
+
+for elem in result:
+    print(elem)
+
+con.close()
